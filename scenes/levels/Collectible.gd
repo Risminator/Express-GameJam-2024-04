e93c97b84fb5c37b1b92196a3ca743505a198e2c
Risminator <118983698+Risminator@users.noreply.github.com>
2024-04-06 15:40:07 +0300

    fix: working rotation

--- scenes/levels/Collectible.gd
+++ scenes/levels/Collectible.gd
@@ -1,13 +1,16 @@
 extends Area2D
 
+func setRandomSprite():
+	var frames = $SkullSprite.texture.get_width() / $SkullSprite.region_rect.size.x
+	var random_index = randi_range(0, frames - 2)
+	$SkullSprite.region_rect.position.x = random_index * $SkullSprite.region_rect.size.x
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
-
-	
+func _ready():
+	setRandomSprite()
 
 
 func _on_body_entered(body):
