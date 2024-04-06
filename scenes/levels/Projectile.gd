cb577c8ba9460110455e5fc380414e6973dada27
Local Piper <artsor53@gmail.com>
2024-04-06 16:44:35 +0300

    feat: added even more bugs

--- scenes/levels/Projectile.gd
+++ scenes/levels/Projectile.gd
@@ -1,14 +1,10 @@
 extends Area2D
 
+@export var speed = 2
+var direction = Vector2(1,1)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	global_position += direction
 
 
 
