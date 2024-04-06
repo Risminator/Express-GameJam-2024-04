685f525c85aa8475f515ac9b6ad71271e91274ea
Local Piper <artsor53@gmail.com>
2024-04-06 14:55:13 +0300

    chore: added a bug

--- scenes/levels/Robot.gd
+++ scenes/levels/Robot.gd
@@ -1,13 +1,18 @@
-extends AnimatableBody2D
+extends Node2D
 
 var d = 0
+@export var coef_x = 1
+@export var coef_y = 1
 @export var radius = 100
 var speed = 2
 @export var time_modifier = 4
+var parent = null
+
+
 func _process(delta):
-	d += delta * time_modifier
-	print(get_parent())
+	parent = get_parent()
+	d += delta
 	position = Vector2(
-		sin(d + speed) * radius,
-		cos(d + speed) * radius
+		coef_x + sin(d) * radius,
+		coef_y + cos(d) * radius
 	)
